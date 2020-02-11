088cc8298bcdc46fb25d67563d192d926f0aea0f
Westefns Damião de Souza Silva <l4304@liga.org.br>
2020-02-11 11:30:18 -0300

    Removendo um item da tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
     this.todos.push(new Todo(1, 'passer com meu cachorro', true));
     this.todos.push(new Todo(2, 'ir ao supermecado', false));
   }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {}
+
+  markAsUndone() {}
 }
