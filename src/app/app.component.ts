939f336ab96e64526ab0cd021eb7322665c7aa5f
Westefns Damião de Souza Silva <l4304@liga.org.br>
2020-02-19 12:44:14 -0300

    Adicionando Tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,17 @@ export class AppComponent {
         Validators.required
       ])]
     });
+  }
 
-    this.todos.push(new Todo(1, 'passer com meu cachorro', true));
-    this.todos.push(new Todo(2, 'ir ao supermecado', false));
+  add() {
+    const title = this.form.controls["title"].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
